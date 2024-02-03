aa527bf0db5c8a546bcaed07d42e4c00ec635484
Ishmum15 <124346030+Ishmum15@users.noreply.github.com>
2024-02-03 16:35:42 +0600

    Delete ISHMUM.py

--- ISHMUM.py
+++
@@ -1,6 +0,0 @@
-import os,platform
-os.system('git pull')
-#exit('\n Update Soon...!')
-ISHMUM=platform.architecture()[0]
-if ISHMUM=="32bit":__import__("ISHMUM32")
-elif ISHMUM=="64bit":__import__("ISHMUM")
